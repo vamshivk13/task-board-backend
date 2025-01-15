3f1d6100bf1c3fcb06f2bf18e691032412764d26
thatikondavamshi13 <thatikondavamshi13@gmail.com>
2025-01-16 02:08:51 +0530

    origin updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://task-board-ui-zeta.vercel.app/"
+    "https://task-board-ui-zeta.vercel.app"
   ); // Allow all origins
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow methods
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow headers
