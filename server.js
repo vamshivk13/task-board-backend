277e25a0bd7e0b2b261beead2464e18ad4e5c29a
thatikondavamshi13 <thatikondavamshi13@gmail.com>
2025-01-28 10:33:16 +0530

    env added

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://task-board-ui-zeta.vercel.app"
-  ); // Allow all origins
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL); // Allow all origins
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow methods
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow headers
   res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials (cookies, auth headers)
